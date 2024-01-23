616b4948060c9eb05fba9fca4aa9e4f82ec4d953
lorenzoo014 <lagudmarr@gmail.com>
2024-01-23 18:07:42 +0100

    Personalizando las variables

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../src/App';
+import App from '../src/index';
 
 test('renderiza la aplicación correctamente', () => {
   render(<App />);
--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
