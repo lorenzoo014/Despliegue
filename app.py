6887995f7772a5d4f0b694b23294e48bdec8ab83
lorenzoo014 <lagudmarr@gmail.com>
2024-01-24 17:58:44 +0100

    Casi esta

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify,request
+from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/startMLflowScript', methods=['POST'])
+@app.route('/start_mlflow_script', methods=['POST'])
 def start_mlflow_script():
-    try:
-        # Ejecutar mlflow_script.py como un proceso separado
-        subprocess.run(['python', 'mlflow_script.py'])
+    # try:
+    # 
+    #     # Ejecutar mlflow_script.py como un proceso separado
+    #     subprocess.run(['python', 'mlflow_script.py'])
+    #     return jsonify({'message': 'MLflow script started successfully'})
+    # except Exception as e:
+    #     return jsonify({'error': str(e)})
+    # return jsonify({'message': 'Hello from start_mlflow_script'})
+    if request.method == 'POST':
+        # Aquí puedes poner el código para ejecutar tu script de MLflow
+        # subprocess.run(['python', 'mlflow_script.py'])
         return jsonify({'message': 'MLflow script started successfully'})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    if request.method == 'POST':
+        return jsonify({'message': 'Hola_hpoa'})
+    else:
+        return jsonify({'message': 'Hello from start_mlflow_script'})
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
